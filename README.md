bd28220b588563a201ced1f96dd1f557df5713f3
gruand <gruand@gmail.com>
2024-03-13 15:06:34 +0200

    Insert new students to DB

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-### Student management system with DB and GUI on PyQt6
+## Student management system with DB and GUI on PyQt6
+
+### In example.py - basics of working with GUI elements in PyQt6
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, \
-    QTableWidget, QTableWidgetItem
+    QTableWidget, QTableWidgetItem, QDialog, QVBoxLayout, QComboBox
 import sys
 import sqlite3
 
@@ -9,10 +9,13 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Students Management")
+        self.setFixedWidth(640)
+        self.setFixedHeight(400)
 
         file_menu_item = self.menuBar().addMenu("&File")
 
         add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         help_menu_item = self.menuBar().addMenu("&Help")
@@ -24,18 +27,71 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Mobile"))
-        self.table.verticalHeader().setVisible(False) # Hide column with default IDs
+        self.table.verticalHeader().setVisible(False)  # Hide column with default IDs
         self.setCentralWidget(self.table)
 
     def load_data(self):
         connection = sqlite3.connect("database.db")
         result = connection.execute("SELECT * FROM students")
-        self.table.setRowCount(0) # Reset the table and load it again fresh
+        self.table.setRowCount(0)  # Reset the table and load it again fresh
         for row_number, row_data in enumerate(result):
             self.table.insertRow(row_number)
             for column_number, column_data in enumerate(row_data):
                 self.table.setItem(row_number, column_number, QTableWidgetItem(str(column_data)))
         connection.close()
+
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Add Student")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        # Add Student's name widget
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Enter Student's Name")
+        layout.addWidget(self.student_name)
+
+        # Choose Course widget
+        self.course_name = QComboBox()
+        courses = ["Astronomy", "Biology", "Maths", "Physics"]
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        # Add Mobile
+        self.mobile = QLineEdit()
+        self.mobile.setPlaceholderText(" Student's Mobile Phone")
+        layout.addWidget(self.mobile)
+
+        # Add submit button
+        button = QPushButton("Add Student")
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.mobile.text()
+        
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
+                       (name, course, mobile))
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        # Refresh data in table from DB
+        window.load_data()
 
 
 app = QApplication(sys.argv)
